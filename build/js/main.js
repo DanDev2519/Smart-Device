258c94d4c2fb6fe0c0127d25a5b71594d6864062
DanDev2519 <ostryakov.danil@yandex.ru>
2020-10-28 22:07:02 +0300

    Styling questions block for desktop

---
+++ build/js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// var navMain = document.querySelector('.main-nav');
+// var navToggle = document.querySelector('.main-nav__toggle');
+
+// navMain.classList.remove('main-nav--nojs');
+// navMain.classList.remove('main-nav--opened');
+// navMain.classList.add('main-nav--closed');
+
+// navToggle.addEventListener('click', function () {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
+
+// Проверка поддержки хранилища
+var isStorageSupport = true;
+var storageName = '';
+var storageTel = '';
+
+try {
+  storageName = localStorage.getItem('name');
+  storageTel = localStorage.getItem('tel');
+} catch (err) {
+  isStorageSupport = false;
+}
